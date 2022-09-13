6705d7b7181e3de4f9cb1e8b1d3aa6b4563f519e
MomDra <76085940+MomDra@users.noreply.github.com>
2022-09-13 13:46:02 +0900

    Flip camera

--- MediaPipeUnityPlugin-0.10.1_02/Assets/MediaPipeUnity/Tutorial/Official Solution/FaceMesh.cs
+++ MediaPipeUnityPlugin-0.10.1_02/Assets/MediaPipeUnity/Tutorial/Official Solution/FaceMesh.cs
@@ -49,8 +49,10 @@ namespace Mediapipe.Unity.Tutorial
 
       _screen.rectTransform.sizeDelta = new Vector2(_width, _height);
 
+      // MediaPipe에서, CPU에 있는 이미지 데이터는 ImageFrame 클래스에 저장됨
       _inputTexture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
       _inputPixelData = new Color32[_width * _height];
+
       _outputTexture = new Texture2D(_width, _height, TextureFormat.RGBA32, false);
       _outputPixelData = new Color32[_width * _height];
       
@@ -64,25 +66,36 @@ namespace Mediapipe.Unity.Tutorial
       var stopwatch = new Stopwatch();
       
       _graph = new CalculatorGraph(_configAsset.text);
+      // configAsset을 통해 그래프 생성
+
       var outputVideoStream = new OutputStream<ImageFramePacket, ImageFrame>(_graph, "output_video");
+      // outputstream을 얻기위한 새로운 방법
       outputVideoStream.StartPolling().AssertOk();
+      // 그래프를 시작하기 전에 startpolling()함수를 호출해야 함
 
       _graph.StartRun().AssertOk();
 
       stopwatch.Start();
+      // timestamp를 알기 위해 사용되는 stopwatch임
 
       while (true)
       {
         _inputTexture.SetPixels32(_webCamTexture.GetPixels32(_inputPixelData));
+        // ImageFrame 객체는 NativeArray<bytes>를 이용해서 초기화 할 수 있음
+        // webCamTexture 데이터를 Texture2D로 복사하는 거임, NativeArray<byte>를 얻으려고
         var imageFrame = new ImageFrame(ImageFormat.Types.Format.Srgba, _width, _height, _width * 4, _inputTexture.GetRawTextureData<byte>());
+        // 픽셀 값과 다음 행의 동일한 픽셀 및 채널 사이의 바이트 오프셋입니다.
+        // 대부분의 경우, 이는 폭과 채널 수의 곱과 같습니다.
 
-        var currentTimestamp = stopwatch.ElapsedTicks / (System.TimeSpan.TicksPerMillisecond);
+        var currentTimestamp = stopwatch.ElapsedTicks / (System.TimeSpan.TicksPerMillisecond / 1000);
+        // timestamp의 단위는 ms임
 
         _graph.AddPacketToInputStream("input_video", new ImageFramePacket(imageFrame, new Timestamp(currentTimestamp))).AssertOk();
+        // ImageFramePacket을 이용해 그래프에 이미지를 입력함
 
         yield return new WaitForEndOfFrame();
 
-        if (outputVideoStream.TryGetNext(out var outputVideo))
+        if (outputVideoStream.TryGetNext(out var outputVideo)) // true를 반환하면 output을 가져올 수 있음
         {
           if (outputVideo.TryReadPixelData(_outputPixelData))
           {
